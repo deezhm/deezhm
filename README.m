cab56c108562954ffbd106b3852d2cd87266ac83
deezhm <104109690+deezhm@users.noreply.github.com>
2022-04-20 19:43:07 -0400

    Create README.m
    .

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @deezhm
+- 👀 I’m interested in coding/scripting
+-  currrently learning sctiping/coding 
+- 💞️ I’m looking to collaborate on roblox scipts 
+- 📫 How to reach me add my discord "cheetopuff#6981
+
+<!---
+deezhm/deezhm is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
